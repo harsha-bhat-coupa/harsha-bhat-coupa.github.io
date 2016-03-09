84779a91f135e28b19d8478cbadfce292635a381
Harsha Bhat <harshabhat86@gmail.com>
2016-03-09 13:48:15 -0800

    Initial Commit

---
+++ js/minify.js
@@ -0,0 +1,10 @@
+var minify = require('gulp-minify');
+
+gulp.task('compress', function() {
+      gulp.src('lib/*.js')
+          .pipe(minify({
+                      exclude: ['tasks'],
+                              ignoreFiles: ['.combo.js', '-min.js']
+                                  }))
+                                      .pipe(gulp.dest('dist'))
+});
